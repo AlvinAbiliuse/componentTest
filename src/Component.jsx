8a2c49069d2beea0cac6611a2e788fee192437a6
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-10 14:43:30 +0400

    Component.jsx

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,143 +1,17 @@
+import "./styles.css";
+
 export function Component() {
 	return (
 		<>
-			<div
-				style={{
-					width: "100%",
-					height: "100%",
-					position: "relative",
-					background: "#6A6565",
-					overflow: "hidden",
-				}}
-			>
-				<div
-					style={{
-						width: 370,
-						height: 337,
-						paddingLeft: 50.74,
-						paddingRight: 50.74,
-						paddingTop: 36.39,
-						paddingBottom: 36.39,
-						left: 10,
-						top: 309,
-						position: "absolute",
-						background:
-							"linear-gradient(0deg, rgba(0, 0, 0, 0.46) 0%, rgba(0, 0, 0, 0.46) 100%)",
-						overflow: "hidden",
-						backgroundImage: "url(https://placehold.co/370x337)",
-						flexDirection: "column",
-						justifyContent: "center",
-						alignItems: "center",
-						gap: 5.12,
-						display: "inline-flex",
-					}}
-				>
-					<div
-						style={{
-							alignSelf: "stretch",
-							height: 288.53,
-							flexDirection: "column",
-							justifyContent: "flex-start",
-							alignItems: "center",
-							gap: 33.82,
-							display: "flex",
-						}}
-					>
-						<div
-							style={{
-								alignSelf: "stretch",
-								height: 186.54,
-								flexDirection: "column",
-								justifyContent: "flex-start",
-								alignItems: "flex-start",
-								gap: 15.37,
-								display: "flex",
-							}}
-						>
-							<div
-								style={{
-									alignSelf: "stretch",
-									height: 35.87,
-									color: "rgba(255, 255, 255, 0.91)",
-									fontSize: 32.8,
-									fontFamily: "Inter",
-									fontWeight: "500",
-									letterSpacing: 0.66,
-									wordWrap: "break-word",
-								}}
-							>
-								TITLE
-							</div>
-							<div
-								style={{
-									width: 268,
-									height: 168,
-									color: "white",
-									fontSize: 20.5,
-									fontFamily: "Inter",
-									fontWeight: "400",
-									lineHeight: 24.83,
-									letterSpacing: 0.2,
-									wordWrap: "break-word",
-								}}
-							>
-								Lorem ipsum dolor sit amet consectetur. Nunc pharetra non duis
-								nunc nisl eleifend faucibus libero. Integer magna sit neque
-								sociis donec. Hendrerit libero dolor nisi in.
-							</div>
-						</div>
-						<div
-							style={{
-								alignSelf: "stretch",
-								height: 58.94,
-								paddingLeft: 38.44,
-								paddingRight: 38.44,
-								paddingTop: 13.32,
-								paddingBottom: 13.32,
-								background: "rgba(217, 217, 217, 0.10)",
-								borderRadius: 55.35,
-								outline: "0.51px rgba(0, 0, 0, 0.52) solid",
-								backdropFilter: "blur(5.12px)",
-								justifyContent: "space-between",
-								alignItems: "center",
-								display: "inline-flex",
-							}}
-						>
-							<div
-								style={{
-									color: "rgba(240.90, 238.87, 238.87, 0.90)",
-									fontSize: 21.52,
-									fontFamily: "Inter",
-									fontWeight: "300",
-									letterSpacing: 2.58,
-									wordWrap: "break-word",
-									textShadow: "10px 2px 21px rgba(3, 3, 3, 0.69)",
-								}}
-							>
-								ACTION BUTTON!
-							</div>
-						</div>
-					</div>
-				</div>
-				<div
-					style={{
-						width: 354,
-						height: 117,
-						left: 18,
-						top: 185,
-						position: "absolute",
-						color: "white",
-						fontSize: 40,
-						fontFamily: "Inter",
-						fontWeight: "400",
-						letterSpacing: 4.8,
-						wordWrap: "break-word",
-					}}
-				>
-					Membership, and other stuff
-				</div>
+			<h1>Membership, and other stuff</h1>
+			<div className="card">
+				<h2>Membership</h2>
+				<p>
+					Become part of the club, explore unique offers, and enjoy uparralleled
+					access to our exclusive club.
+				</p>
+				<button className="actionBtn">ACTION BUTTON!</button>
 			</div>
-			;
 		</>
 	);
 }
